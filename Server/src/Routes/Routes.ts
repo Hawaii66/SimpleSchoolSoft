99bfe55e300acd85e63224dad536b78359159d5b
Sebastian Ahlman <hawaiilive@outlook.com>
2022-03-12 11:20:25 +0100

    Simple express REST api

---
+++ Server/src/Routes/Routes.ts
@@ -0,0 +1,36 @@
+import {Express} from "express"
+import puppeteer from "puppeteer";
+import { GetLunchMenu, GetNextLesson } from "../Utils/SchoolSoft";
+
+export const Routes = (app:Express) => {
+    app.get("/lunch", async(req,res)=>{
+        const {username, password} = req.query;
+
+        if(typeof username !== "string" || typeof password !== "string")
+        {
+            return res.status(400).send("Not valid username or password");
+        }
+
+        const browser = await puppeteer.launch({headless:true});
+        var result = await GetLunchMenu(browser,username,password);
+
+        res.json(result);
+    });
+
+    app.get("/nextlesson", async(req,res)=>{
+        const {username, password} = req.query;
+
+        if(typeof username !== "string" || typeof password !== "string")
+        {
+            return res.status(400).send("Not valid username or password");
+        }
+        
+        const browser = await puppeteer.launch({headless:true});
+        var result = await GetNextLesson(browser, username, password);
+        res.json(result);
+    });
+
+    app.get("/",(req,res)=>{
+        res.status(200).send("Hello World!")
+    });
+}
---
+++ Server/src/Utils/SchoolSoft.ts
@@ -0,0 +1,204 @@
+import puppeteer from "puppeteer";
+import { ILesson, ILunch } from "../Interfaces/Schoolsoft";
+require('dotenv').config()
+
+const GetCurrentPage = async (browser:puppeteer.Browser) => 
+{
+    return (await browser.pages())[1];
+}
+
+const Auth = async (browser:puppeteer.Browser, user:string,pass:string) => 
+{
+    const loginURL = "https://sms12.schoolsoft.se/nykopingsenskilda/jsp/Login.jsp"
+    var page = await GetCurrentPage(browser);
+
+    if(page.url() !== loginURL)
+    {
+        await page.goto(loginURL);
+        page = await GetCurrentPage(browser);
+    }
+
+    //Select Elev user
+    await page.select("select[id=usertype]", "1");
+    //await sleep(1000);
+    await page.waitForNetworkIdle();
+    page = await GetCurrentPage(browser);
+
+    const username = await page.$("[id=ssusername]");
+    const password = await page.$("[id=sspassword]");
+    const loginButton = await page.$("[type=submit]");
+    if(!username || !password || !loginButton){return null;}
+
+    await username.type(user);
+    await password.type(pass);
+    await loginButton.click();
+
+    //await sleep(3000);
+    await page.waitForNetworkIdle();
+
+    return await GetCurrentPage(browser);
+}
+
+export const GetLunchMenu = async (browser:puppeteer.Browser,username:string,password:string) => {
+    await browser.newPage();
+
+    var page = await Auth(browser,username,password);
+    if(page === null){return;}
+
+    const menu = await page.$("a[name=lunchmenu]");
+    menu?.click();
+
+    //await sleep(3000);
+    await page.waitForNetworkIdle();
+
+    const lunches =  await page.$$("#lunchmenu_con_content > table");
+
+    var lunch:ILunch[] = [];
+    for(var i = 0; i < lunches.length; i ++)
+    {
+        const td = (await lunches[i].$$("td[valign=top]"))[1];
+        if(!td){return;}
+        var food:string|undefined = (await (await td.getProperty("innerHTML")).jsonValue())
+        if(!food){return;}
+        lunch.push({
+            normal:food.split("<br>")[0],
+            veg:(food.split("<br>")[1]).replace("Veg:","").trim(),
+            time:new Date().getTime(),
+            img:""
+        });
+    }
+
+    var promises:Promise<string>[] = [];
+    for(var i = 0; i < lunch.length; i ++)
+    {
+        const image = (browser:puppeteer.Browser, query:string) => {
+            return new Promise<string>(async(resolve,reject)=>{
+                const page = await browser.newPage();
+                await page.goto(`https://www.google.com/search?q=${query}&source=lnms&tbm=isch`)
+
+                const parent = await page.$("div.islrc");
+                const firstImg = await parent?.$("div > a > div > img");
+
+                if(!firstImg){return reject("")}
+
+                resolve(await(await firstImg.getProperty("src")).jsonValue())
+            });
+        }
+
+        promises.push(image(browser, lunch[i].normal));
+    }
+
+    return await Promise.all<string>(promises).then(res=>{
+        for(var i = 0; i < res.length; i ++)
+        {
+            lunch[i].img = res[i] || "";
+        }
+        return lunch
+    });
+}
+
+export const GetNextLesson = async (browser:puppeteer.Browser,user:string,pass:string) => {
+    var page = await browser.newPage();
+    await page.goto("https://sms12.schoolsoft.se/nykopingsenskilda/jsp/Login.jsp");
+
+    await page.select("select[id=usertype]", "1");
+    //await sleep(1000);
+    await page.waitForNetworkIdle();
+
+    page = (await browser.pages())[1];
+
+    const username = await page.$("[id=ssusername]")
+    const password = await page.$("[id=sspassword]")
+    const loginButton = await page.$("[type=submit]")
+    if(username === null || password === null){return;}
+
+    await username.type(user)
+    await password.type(pass)
+    await loginButton?.click();
+
+    //await sleep(3000);
+    await page.waitForNetworkIdle();
+
+    page = (await browser.pages())[1];
+
+    const scheduleButton = await page.$("[id=menu_schedule]");
+    await scheduleButton?.click();
+
+    //await sleep(1000);
+    await page.waitForNetworkIdle();
+
+    page = (await browser.pages())[1];
+
+    const schedules = await page.$$('a.schedule')
+
+    const currentDate = new Date();
+    const currentDay = currentDate.getDay()
+    var currentMin = currentDate.getMinutes() + currentDate.getHours() * 60;
+    
+    var minDiff = 100000;
+    var lesson:ILesson = {
+        hour:0,
+        minute:0,
+        name:"",
+        sal:"",
+        color:""
+    }
+
+    for(var i = 0; i < schedules.length; i ++){
+        const eng = await schedules[i].$("span");
+        if(!eng){return;}
+        await eng.click();
+        await sleep(250)
+        const lectionInfo = await page.$("[id=lessonInfo_content");
+        const details:string|undefined = (await(await lectionInfo?.getProperty("innerHTML"))?.jsonValue());
+        if(!details){return;}
+        const date = details.includes("mån") ? 6 : 
+            details.includes("tis") ? 2 :
+            details.includes("ons") ? 3 :
+            details.includes("tor") ? 4 :
+            details.includes("fre") ? 5 :
+            details.includes("lör") ? 6 : 0
+
+        if(currentDay === date)
+        {
+            var timeDetail = details.split(" ")[0];
+            const time:{hour:number,min:number} = {
+                hour:parseInt(timeDetail.split(":")[0]),
+                min:parseInt(timeDetail.split(":")[1])
+            }
+            const minutes = time.hour * 60 + time.min
+
+            var diff = minutes - currentMin;
+            if(diff > 0 && diff < minDiff)
+            {
+                minDiff = diff;
+                
+                const color = await schedules[i].$eval("span", el =>
+                    getComputedStyle(el).backgroundColor
+                );
+
+                lesson = {
+                    hour:time.hour,
+                    minute:time.min,
+                    name:details.split("<br>")[1],
+                    sal:details.split("<br>")[2],
+                    color:color
+                }
+            }
+        }
+
+        const closeButton = await page.$("button[title=close]");
+        await closeButton?.click();
+    }
+
+    return lesson;
+};
+
+
+
+function sleep(ms:number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
