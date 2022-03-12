2880905eecc2879aa17a9430095cf44f3daa04d4
Sebastian Ahlman <hawaiilive@outlook.com>
2022-03-12 23:59:25 +0100

    Server endpoints

--- simple-schoolsoft/src/Contexts/StaticContext.tsx
+++ simple-schoolsoft/src/Contexts/StaticContext.tsx
@@ -8,8 +8,8 @@ interface StaticContextInterface {
 
 export const StaticContext = createContext<StaticContextInterface>(
     {
-        apiEndPoint:"http://localhost:5000",//*/"https://hawaii-dev-blog.herokuapp.com",
-        website:"http://localhost:3000",//*/"https://www.blog.hawaiidev.net",
-        debug:true//*/false
+        apiEndPoint:/*"http://localhost:5000",//*/"https://simpleschoolsoft.herokuapp.com",
+        website:/*"http://localhost:3000",//*/"https://www.simple.hawaiidev.net",
+        debug:false//*/false
     }
 );
